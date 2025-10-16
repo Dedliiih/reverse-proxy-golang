b7f958cadaf80d3a1e6f2d1d54b3871c11024a57
Benjamin <benjafu78@gmail.com>
2025-10-16 12:36:53 -0300

    agregar reverse proxy y logs

--- main.go
+++ main.go
@@ -3,20 +3,39 @@ package main
 import (
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"time"
 )
 
-func handleRoot(w http.ResponseWriter, r *http.Request) {
+func handleHello(w http.ResponseWriter, r *http.Request) {
 	log.Println("Request received:", r.Method, r.URL.Path)
 	
 	w.Write([]byte("Hello World"))
 }
 
+func setupProxy() *httputil.ReverseProxy {
+    log.Println("Setting up reverse proxy to NestJS server at http://localhost:3000")
+
+	target, err := url.Parse("http://localhost:3000")
+
+	if err != nil {
+		log.Fatal("Error parsing target URL:", err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(target)
+	return proxy
+}
+
 func main() {
     mux := http.NewServeMux()
+    proxy := setupProxy()
 
-	mux.HandleFunc("/", handleRoot)
 
+	mux.Handle("/", proxy)
+	mux.HandleFunc("/hello", handleHello)
+
+	
 	server := &http.Server{
 		Addr: ":8080",
 		Handler: mux,
